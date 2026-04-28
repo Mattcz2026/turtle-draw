c3cf26376fd9582d494f1ed58c2bfb18d14d67f0
Mattcz2026 <matthewjczaja@lewisu.edu>
2026-04-28 17:05:45 -0500

    Update TurtleDraw_MC.py

--- TurtleDraw_MC.py
+++ TurtleDraw_MC.py
@@ -9,3 +9,40 @@ edTheTurtle = turtle.Turtle()
 edTheTurtle.forward(100)
 
 turtle.done()
+
+
+spiral = turtle.Turtle()
+spiral.pencolor("purple")
+for i in range(40):
+    spiral.forward(i * 10)
+    spiral.rght(144)
+  '''
+
+print('Reading a text file line by line.')
+TurtleDraw = open(TEXTFILENAME, 'r')
+Line = TurtleDraw.readline()
+
+while line:
+    print(line, end='')
+  line = turtleDraw
+
+  if(len(parts) == 3):
+  color = parts[0]
+  x = int(parts[1])
+  y = int(parts[2])
+
+
+  TurtleDraw.color(color)
+  TurtleDraw.goto(x,y)
+  TurtleDraw.pendown()
+
+
+  draw.pendown()
+  draw.goto(x,y)
+
+  Line = TurtleDraw.readline()
+
+  turtle.done()
+
+  #Wait to press the enter key button
+  print('\nEnd')
